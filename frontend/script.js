ea031e2454963b1a53e54c96ca9a523a4c0443a2
agnusdeivera-ops <agnusdeivera@gmail.com>
2026-03-17 20:02:55 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,19 @@
 document.getElementById("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const res = await fetch("http://localhost:5000/prenotazioni", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      nome: document.getElementById("nome").value,
-      tipo_visita: document.getElementById("visita").value,
-      data: document.getElementById("data").value,
-      orario: document.getElementById("orario").value,
-      medico: "Dr. Rossi"
-    })
-  });
-  const json = await res.json();
-  alert(JSON.stringify(json));
+    e.preventDefault();
+    const res = await fetch("/prenotazioni", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            nome: document.getElementById("nome").value,
+            cognome: document.getElementById("cognome").value,
+            data_nascita: document.getElementById("data_nascita").value,
+            cf: document.getElementById("cf").value,
+            tipo_visita: document.getElementById("visita").value,
+            data: document.getElementById("data").value,
+            orario: document.getElementById("orario").value,
+            medico: "Dr. Rossi"
+        })
+    });
+    const json = await res.json();
+    alert(JSON.stringify(json));
 });
